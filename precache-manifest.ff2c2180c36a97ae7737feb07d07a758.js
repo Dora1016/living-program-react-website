f3482063e1f694da80f6cb72a3f37daa98312992
Dora <939217129@qq.com>
2022-04-22 22:19:44 +0800

    init

---
+++ precache-manifest.ff2c2180c36a97ae7737feb07d07a758.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "372b18f3cea21fe7d467e87df969c01f",
+    "url": "/living-program-react-website/index.html"
+  },
+  {
+    "revision": "251489103d9cd5355370",
+    "url": "/living-program-react-website/static/css/main.da6b37b3.chunk.css"
+  },
+  {
+    "revision": "f906043227e797e78034",
+    "url": "/living-program-react-website/static/js/2.b040eda8.chunk.js"
+  },
+  {
+    "revision": "fc8b115d4064d6797b6def218d457695",
+    "url": "/living-program-react-website/static/js/2.b040eda8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8681bfc5a8c738d4801e",
+    "url": "/living-program-react-website/static/js/3.e1db653e.chunk.js"
+  },
+  {
+    "revision": "251489103d9cd5355370",
+    "url": "/living-program-react-website/static/js/main.f0f2a13b.chunk.js"
+  },
+  {
+    "revision": "f7d8edfde1d6ee726c1e",
+    "url": "/living-program-react-website/static/js/runtime-main.4295bf7b.js"
+  }
+]);
